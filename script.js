9040cb00dafef1b92f2cdbd83abba75b10a8235a
Characterless <0characterless@protonmail.com>
2025-09-23 17:42:29 -0500

    1.3 added javascript stuff teehee :3c

---
+++ script.js
@@ -0,0 +1,7 @@
+function setCommand(cmdString){
+  document.getElementById("command").innerHTML = cmdString;
+}
+
+function resetCommand(){
+  document.getElementById("command").innerHTML = null;
+}
